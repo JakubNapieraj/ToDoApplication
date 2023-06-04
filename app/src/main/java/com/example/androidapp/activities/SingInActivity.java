1f36c1044b2ebf70a8424634b0e4b549f15382d9
iCelo-git <Pr3$tiz123>
2023-06-04 03:39:16 +0200

    Reklamy

--- app/src/main/java/com/example/androidapp/activities/SingInActivity.java
+++ app/src/main/java/com/example/androidapp/activities/SingInActivity.java
@@ -1,6 +1,7 @@
 package com.example.androidapp.activities;
 
 import static com.example.androidapp.AddNewTask.TAG;
+import static com.facebook.FacebookSdk.setAutoLogAppEventsEnabled;
 
 import android.content.Intent;
 import android.content.res.Configuration;
@@ -8,15 +9,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.androidapp.BuildConfig;
 import com.example.androidapp.R;
 import com.example.androidapp.databinding.ActivitySingInBinding;
 import com.example.androidapp.utilities.Constants;
 import com.example.androidapp.utilities.PreferenceManager;
+import com.facebook.ads.AdSettings;
+import com.facebook.ads.AdSize;
+import com.facebook.ads.AdView;
+import com.facebook.ads.AudienceNetworkAds;
+import com.facebook.ads.NativeAd;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.LoggingBehavior;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -29,7 +41,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class SingInActivity extends AppCompatActivity {
 
     private ActivitySingInBinding binding;
+    private AdView adView;
+    private NativeAd nativeAd;
     private PreferenceManager preferenceManager;
+
     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
@@ -53,6 +68,37 @@ public class SingInActivity extends AppCompatActivity {
         binding = ActivitySingInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
+//
+        AudienceNetworkAds.initialize(this);
+        AdSettings.setDebugBuild(BuildConfig.DEBUG);
+        AdSettings.addTestDevice("5ca2a4f6");
+        boolean shouldCollectAdvertiserId = true; // Set this based on user consent
+        String[] dataProcessingOptions = shouldCollectAdvertiserId ? new String[]{"LDU"} : null;
+        AdSettings.setDataProcessingOptions(dataProcessingOptions);
+////        AppEventsLogger.setPushNotificationsRegistrationEnabled(true);
+////        AppEventsLogger.setAutoLogAppEventsEnabled(true);
+////        AppEventsLogger.setAdvertiserIDCollectionEnabled(true);
+//
+//        nativeAd = new NativeAd(this, "VID_HD_9_16_39S_APP_INSTALL#{your-placement-id}");
+//
+        setAutoLogAppEventsEnabled(true);
+        adView = new AdView(this, "IMG_16_9_APP_INSTALL#YOUR_PLACEMENT_ID", AdSize.BANNER_HEIGHT_50);
+        LinearLayout adContainer = (LinearLayout) findViewById(R.id.banner_container);
+        adContainer.addView(adView);
+//        adView.loadAd();
+//        nativeAd.loadAd();
+
+
+        if (BuildConfig.DEBUG) {
+            AdSettings.setTestMode(true);
+        }
+        com.facebook.ads.AdView adView;
+        adView = new com.facebook.ads.AdView(this, "PLACEMENT-ID",
+                com.facebook.ads.AdSize.BANNER_HEIGHT_50);
+
+        ((LinearLayout) adContainer).addView(adView);
+        adView.loadAd();
+
     }
 
     private void setListeners() {
@@ -136,6 +182,14 @@ private void firebaseLogin() {
                 }
             });
 }
+
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
+    }
     private boolean isDarkModeOn() {
         int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
         return currentNightMode == Configuration.UI_MODE_NIGHT_YES;
